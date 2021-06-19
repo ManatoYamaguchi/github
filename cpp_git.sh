eef20e7124e6955e3eb4463780d6b61a36f99f1c
ManatoYamaguchi <S19C1117GP@s.chibakoudai.jp>
2021-06-19 23:32:00 +0900

    Add a file

---
+++ cpp_git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#実行ファイルの削除
+rm a.out
+
+git add .
+git status
+
+judg="n"
+
+while ["$judg" = "n"]
+do
+	echo "上記の内容でファイルをpushします。\n"
+	echo "よろしいですか？   ...   Y:yes / N:no\n"
+	read judg
+done
+
+git commit -m "Add a file"
+git push
